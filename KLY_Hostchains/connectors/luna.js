c8384e0933325dd216daa0d4663a75c0a19d9eb7
Vlad Chernenko <vlad1.chernenko2015@gmail.com>
2022-01-28 21:18:01 +0200

    🌜Terra hostchain connector(via tracks) added🌕

--- KLY_Hostchains/connectors/luna.js
+++ KLY_Hostchains/connectors/luna.js
@@ -31,27 +31,97 @@ Links:[
     https://terra.stake.id/#/ - Explorer 1
     https://terra-money.github.io/terra.js/ - micro docs
     https://docs.terra.money/SDKs/Terra-js/Smart-contracts.html - for future TYPEs of interaction with hostchain
+    https://github.com/terra-money/terra.js/wiki/WebSockets
+
 ]
 
 */
 
+
+import {LCDClient,MsgSend,MnemonicKey} from '@terra-money/terra.js'
+
+import {LOG} from '../../KLY_Space/utils.js'
+
+
+
+
+
+/**
+ * 
+ * ________________Add separate thread of connection for each symbiote________________
+ * 
+ * 
+ */
+let connections=new Map()
+
+Object.keys(CONFIG.CHAINS).forEach(
+     
+    symbiote => {
+ 
+        let {URL,CHAIN_ID}=CONFIG.CHAINS[symbiote].HC_CONFIGS.luna
+         
+        if(configs) connections.set(symbiote,new LCDClient({URL,chainID:CHAIN_ID}))
+ 
+    }
+     
+)
 
 
 
 export default {
 
 
-    checkTx:(hostChainHash,blockIndex,klyntarHash,chainId)=>{
+    checkTx:async(hostChainHash,blockIndex,klyntarHash,chainId)=>{
+      
+        let tx=await connections.get(chainId).tx.txInfo(hostChainHash).then(data=>data.tx).catch(e=>false)
+
+        if(tx){
+
+            let [index,hash]=tx.body.memo.split('_')
+                
+            return index==blockIndex && hash===klyntarHash
+
+        }
 
     },
+
+
 
     sendTx:(chainId,blockIndex,klyntarHash)=>{
-        
-    },
 
+        let {MNEMONIC,TO,AMOUNT}=CONFIG.CHAINS[chainId].HC_CONFIGS.luna,
+
+            account=new MnemonicKey({mnemonic:MNEMONIC}),
+
+            send = new MsgSend(account.accAddress,TO,{uluna:AMOUNT}),
+
+            terra=connections.get(chainId),
+
+            wallet=terra.wallet(account)
+
+
+
+
+        return wallet.createAndSignTx({
+    
+            msgs: [send],
+    
+            memo: blockIndex+'_'+klyntarHash,
+
+        }).then(tx=>
+            
+            terra.tx.broadcast(tx)
+            
+        ).then(result=>result.txhash).catch(e=>LOG(`Some error has been occured in LUNA \x1b[36;1m${e}`,'W'))
+
+    },
+  
 
     //Only for Controller(at least in first releases)
     changeManifest:manifest=>{
 
-    }
+    },
+
+    
+    getBalance:async symbiote=>connections.get(symbiote).bank.balance(symbiote).then(bal=>bal[0].toAmino()[0].amount/10**6)
 }
