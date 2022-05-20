ea1ca74f0022a8398961fea3bebd77cc4ac36add
Vlad Chernenko <vladchernenko@protonmail.com>
2022-05-21 01:27:57 +0300

    Falcon512 added

--- KLY_Addons/falcon512/src/lib.rs
+++ KLY_Addons/falcon512/src/lib.rs
@@ -4,26 +4,40 @@ use pqcrypto_traits::sign::*;
 
 use hex::encode;
 
-
+use js_sys;
 
 
 #[no_mangle]
-pub extern "C" fn genKeys(){
+pub extern "C" fn generate_falcon() -> js_sys::Uint8Array {
 
-    println!("Hello, world!");
-
-    let message = vec![0, 1, 2, 3, 4, 5];
-    let (pk, sk) = keypair();
+    let (public_key, secret_key) = keypair();
     
-    println!("Public => {:?}",encode(&PublicKey::as_bytes(&pk)));
-    println!("\n\nSecret => {:?}",encode(&SecretKey::as_bytes(&sk)));
+    js_sys::Uint8Array::from(&[PublicKey::as_bytes(&public_key),SecretKey::as_bytes(&secret_key)].concat()[..])
+    
+}
 
-    let sm = sign(&message, &sk);
 
-    println!("\n\nSignature => {:?}",encode(&SignedMessage::as_bytes(&sm)));
+#[no_mangle]
+pub extern "C" fn sign_falcon(raw_private:&[u8],data:&[u8]) -> js_sys::Uint8Array{
 
-    let verifiedmsg = open(&sm, &pk).unwrap();
+    //Derive private key
+    let private_key=SecretKey::from_bytes(&raw_private).unwrap();
 
-    println!("\n\nVerified => {:?}",verifiedmsg);
+    let signed_message = sign(&data, &private_key);
+
+    js_sys::Uint8Array::from(SignedMessage::as_bytes(&signed_message))
+
+}
+
+#[no_mangle]
+pub extern "C" fn verify_falcon(raw_pubkey:&[u8],signa:&[u8]) -> js_sys::Uint8Array {
+
+    //Derive public key & signed message
+    let public_key=PublicKey::from_bytes(&raw_pubkey).unwrap();
+    let signed_message=SignedMessage::from_bytes(&signa).unwrap();
+
+    //If verified - the initial signed message will be returned
+    js_sys::Uint8Array::from(&open(&signed_message,&public_key).unwrap() as &[u8])
+
 
 }
