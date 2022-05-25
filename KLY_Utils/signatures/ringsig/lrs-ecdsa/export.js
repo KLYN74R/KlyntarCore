e680192de92ccf7689f9acd1ec6c32d221d60aa4
Vlad Chernenko <vladchernenko@protonmail.com>
2022-05-25 06:13:07 +0300

    LRS added

--- KLY_Utils/signatures/ringsig/lrs-ecdsa/export.js
+++ KLY_Utils/signatures/ringsig/lrs-ecdsa/export.js
@@ -1,4 +1,42 @@
-const { sign, verify, link } = require('.');
-const { Wallet } = require("ethers")
+let elliptic_1 = require("elliptic"),
 
-module.exports={sign,verify,link,Wallet}
+    bn = require('bn.js'),
+    
+    ec = new elliptic_1.ec('secp256k1'),
+
+    {sign,verify,link,RingSign} = require('.'),
+    
+    {Wallet} = require("ethers"),
+
+
+    
+    serializeRingSigtoHex=ringSig=>{
+
+        ringSig.I=ringSig.I.encode('hex')
+        ringSig.C=ringSig.C.toBuffer().toString('hex')
+        ringSig.S=ringSig.S.map(x=>x.toBuffer().toString('hex'))
+        ringSig.M=Buffer.from(ringSig.M).toString('hex')
+        ringSig.Ring=ringSig.Ring.map(pub=>pub.getPublic().encode('hex'))
+
+        return Buffer.from(JSON.stringify(ringSig),'utf-8').toString('hex')                  
+
+    }
+
+
+    let deserializeRingSig=ringSig=>{
+
+        ringSig.I=ec.keyFromPublic(ringSig.I,'hex').getPublic()
+        ringSig.C=new bn(ringSig.C,'hex')
+        ringSig.S=ringSig.S.map(x=>new bn(x,'hex'))
+        ringSig.M=new Uint8Array(Buffer.from(ringSig.M,'hex'))
+
+        let hexKeys=ringSig.Ring.map(pub=>'0x'+pub)
+
+        ringSig.Ring=ringSig.Ring.map(pub=>ec.keyFromPublic(pub,'hex'))
+
+
+        return [hexKeys,ringSig]
+
+    }
+
+module.exports={sign,verify,link,Wallet,serializeRingSigtoHex,deserializeRingSig}
