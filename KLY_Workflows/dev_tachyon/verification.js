fbff27d414b8343a91cdefa3cb01d958716da4f5
Vlad Chernenko <vladchernenko@protonmail.com>
2022-08-11 04:41:02 +0300

    Added work with sync operations for dev_tachyon

--- KLY_Workflows/dev_tachyon/verification.js
+++ KLY_Workflows/dev_tachyon/verification.js
@@ -835,7 +835,7 @@ verifyBlock=async block=>{
         let rewardBox={fees:0}
 
 
-        global.SYNC_OPERATION={VALIDATORS:{}}
+        global.SYNC_OPERATIONS={VALIDATORS:{}}
 
 
         //_________________________________________GET ACCOUNTS FROM STORAGE____________________________________________
@@ -904,8 +904,34 @@ verifyBlock=async block=>{
         //____________________________________________PERFORM SYNC OPERATIONS___________________________________________
 
 
-            
+        global.SYNC_OPERATIONS={VALIDATORS:{}}
 
+
+        //Currently we have sync operations only for changes in validators' stuff
+        Object.keys(SYNC_OPERATIONS.VALIDATORS).forEach(pubKey=>{
+
+            let operation = SYNC_OPERATIONS.VALIDATORS[pubKey]
+
+            if(operation==='DELETE'){
+
+                //Delete from general list of VERIFICATION_THREAD
+                SYMBIOTE_META.VERIFICATION_THREAD.VALIDATORS.splice(SYMBIOTE_META.VERIFICATION_THREAD.VALIDATORS.indexOf(pubKey),1)
+
+                //Delete metadata of validator
+                delete SYMBIOTE_META.VERIFICATION_THREAD.VALIDATORS_METADATA[pubKey]
+
+
+            }else if (operation==='ADD'){
+
+                //Add to general list of VERIFICATION_THREAD
+                SYMBIOTE_META.VERIFICATION_THREAD.VALIDATORS.push(pubKey)
+
+                //Add metadata of validator
+                SYMBIOTE_META.VERIFICATION_THREAD.VALIDATORS_METADATA[pubKey]={INDEX:-1,HASH:'Poyekhali!@Y.A.Gagarin',ACTIVE:true}
+
+            }
+
+        })
 
 
         //__________________________________________SHARE FEES AMONG VALIDATORS_________________________________________
