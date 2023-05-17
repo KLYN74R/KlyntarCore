dff32163fb3982b4f25ab98479b45320056ef911
Vlad Chernenko <vladchernenko@protonmail.com>
2023-05-18 00:34:47 +0300

    Change the block structure to add ASP

--- KLY_Workflows/dev_tachyon/essences/block.js
+++ KLY_Workflows/dev_tachyon/essences/block.js
@@ -5,13 +5,15 @@ import {BLAKE3,GET_GMT_TIMESTAMP} from '../../../KLY_Utils/utils.js'
 
 export default class Block{
     
-    constructor(transactionsSet){
+    constructor(transactionsSet,extraData={}){
         
         this.creator=global.CONFIG.SYMBIOTE.PUB //block creator(validator) Example:7GPupbq1vtKUgaqVeHiDbEJcxS7sSjwPnbht4eRaDBAEJv8ZKHNCSu2Am3CuWnHjta
 
         this.time=GET_GMT_TIMESTAMP() //UTC timestamp (NOTE:in milliseconds)
 
         this.transactions=transactionsSet //array of transactions,contract calls, services logic,etc.
+
+        this.extraData = extraData //extradata to be added to block. Used mostly to add AGGREGATED_SKIP_PROOF
 
         this.index=global.SYMBIOTE_META.GENERATION_THREAD.NEXT_INDEX //index of block in validator's thread
         
