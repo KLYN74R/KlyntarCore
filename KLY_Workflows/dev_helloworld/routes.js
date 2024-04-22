e8b4cdb65e85e78379d46da92db3eba0901d85dd
Vlad Chernenko <vladchernenko@protonmail.com>
2024-04-22 23:59:08 +0300

    Return to work - going to refactor & make beauty

--- KLY_Workflows/dev_helloworld/routes.js
+++
@@ -1,8 +0,0 @@
-//Just import all needed modules with routes
-// We have only dev_helloworld/routes/test.js routes set, so only one file here. You can add more if you need
-
-['test.js'].forEach(
-    
-    mod => import(`./routes/${mod}`)
-    
-)
