0c97cb621ee8fe61604b3321981e804da613537c
Vlad Chernenko <vladchernenko@protonmail.com>
2024-05-10 16:59:14 +0300

    Added test with required operations for FHE ops to use from VMs

--- KLY_Utils/homomorphic/intro_2.js
+++ KLY_Utils/homomorphic/intro_2.js
@@ -76,6 +76,127 @@ import SEAL from 'node-seal'
     
         console.log("len decoded", decoded.length)
         console.log('decoded', decoded)
+
+
+        homomorphicOperations();
+
+        subtractOperations();
     
 
   })()
+
+
+
+async function homomorphicOperations() {
+  
+    const sealInstance = await SEAL();
+
+
+  const schemeType = sealInstance.SchemeType.bfv;
+  const securityLevel = sealInstance.SecurityLevel.tc128;
+  const polyModulusDegree = 4096;
+  const bitSizes = [36, 36, 37];
+  const bitSize = 20;
+
+  const parms = sealInstance.EncryptionParameters(schemeType);
+  parms.setPolyModulusDegree(polyModulusDegree);
+  parms.setCoeffModulus(
+    sealInstance.CoeffModulus.Create(polyModulusDegree, Int32Array.from(bitSizes))
+  );
+  parms.setPlainModulus(
+    sealInstance.PlainModulus.Batching(polyModulusDegree, bitSize)
+  );
+
+  const context = sealInstance.Context(parms, true, securityLevel);
+
+
+  const keyGenerator = sealInstance.KeyGenerator(context);
+  const publicKey = keyGenerator.createPublicKey();
+  const secretKey = keyGenerator.secretKey();
+  const relinKeys = keyGenerator.createRelinKeys();
+
+
+  const encryptor = sealInstance.Encryptor(context, publicKey);
+  const evaluator = sealInstance.Evaluator(context);
+  const decryptor = sealInstance.Decryptor(context, secretKey);
+
+  const encoder = sealInstance.BatchEncoder(context);
+  const plainText1 = encoder.encode(Int32Array.from([5]));
+  const plainText2 = encoder.encode(Int32Array.from([3]));
+
+
+  const cipherText1 = encryptor.encrypt(plainText1);
+  const cipherText2 = encryptor.encrypt(plainText2);
+
+
+  const sum = sealInstance.CipherText();
+  evaluator.add(cipherText1, cipherText2, sum);
+
+  const product = sealInstance.CipherText();
+  evaluator.multiply(cipherText1, cipherText2, product);
+  //evaluator.relinearizeInplace(product, relinKeys);
+
+  const decryptedSum = decryptor.decrypt(sum);
+  const decryptedProduct = decryptor.decrypt(product);
+
+
+  const decodedSum = encoder.decode(decryptedSum);
+  const decodedProduct = encoder.decode(decryptedProduct);
+
+  console.log('Sum:', decodedSum[0]);     
+  console.log('Product:', decodedProduct[0]); 
+}
+
+
+
+async function subtractOperations() {
+
+
+  const sealInstance = await SEAL();
+
+    const schemeType = sealInstance.SchemeType.bfv;
+    const securityLevel = sealInstance.SecurityLevel.tc128;
+    const polyModulusDegree = 4096;
+    const bitSizes = [36, 36, 37];
+    const bitSize = 20;
+  
+    const parms = sealInstance.EncryptionParameters(schemeType);
+    parms.setPolyModulusDegree(polyModulusDegree);
+    parms.setCoeffModulus(
+      sealInstance.CoeffModulus.Create(polyModulusDegree, Int32Array.from(bitSizes))
+    );
+    parms.setPlainModulus(
+      sealInstance.PlainModulus.Batching(polyModulusDegree, bitSize)
+    );
+  
+    const context = sealInstance.Context(parms, true, securityLevel);
+
+    // Key creating
+  const keyGenerator = sealInstance.KeyGenerator(context);
+  const publicKey = keyGenerator.createPublicKey();
+  const secretKey = keyGenerator.secretKey();
+
+    // Ciphertext of 2 vars
+    const encoder = sealInstance.BatchEncoder(context);
+    const plainText1 = encoder.encode(Int32Array.from([10]));
+    const plainText2 = encoder.encode(Int32Array.from([3]));
+  
+    const encryptor = sealInstance.Encryptor(context, publicKey);
+    const cipherText1 = encryptor.encrypt(plainText1);
+    const cipherText2 = encryptor.encrypt(plainText2);
+  
+    // Sum
+    const difference = sealInstance.CipherText();
+    const evaluator = sealInstance.Evaluator(context);
+    evaluator.sub(cipherText1, cipherText2, difference);
+
+
+    // Decrypt
+    const decryptor = sealInstance.Decryptor(context, secretKey);
+    const decryptedDifference = decryptor.decrypt(difference);
+    const decodedDifference = encoder.decode(decryptedDifference);
+    
+    console.log('Difference:', decodedDifference[0]); // Difference: 7
+  }
+  
+  subtractOperations();
