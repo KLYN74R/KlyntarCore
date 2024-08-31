c79910a87b8147ea7780c8540ca9d5832af723ac
Vlad Chernenko <vladchernenko@protonmail.com>
2024-08-31 14:18:30 +0300

    test(lrps_via_ws):Going to test dev branch with mock txs

--- KLY_Workflows/dev_tachyon/life/block_generation.js
+++ KLY_Workflows/dev_tachyon/life/block_generation.js
@@ -4,11 +4,15 @@ import {BLOCKCHAIN_DATABASES, EPOCH_METADATA_MAPPING, NODE_METADATA, WORKING_THR
 
 import {getQuorumMajority, getQuorumUrlsAndPubkeys} from '../common_functions/quorum_related.js'
 
+import { getUserAccountFromState } from '../common_functions/state_interactions.js'
+
+import {BLOCKCHAIN_GENESIS, CONFIGURATION} from '../../../klyn74r.js'
+
+import bls from '../../../KLY_Utils/signatures/multisig/bls.js'
+
 import {signEd25519} from '../../../KLY_Utils/utils.js'
 
 import {blockLog} from '../common_functions/logging.js'
-
-import {CONFIGURATION} from '../../../klyn74r.js'
 
 import {getAllKnownPeers} from '../utils.js'
 
@@ -16,6 +20,18 @@ import Block from '../structures/block.js'
 
 import fetch from 'node-fetch'
 
+import Web1337 from 'web1337'
+
+
+
+
+let web1337 = new Web1337({
+
+    symbioteID:'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    workflowVersion:0,
+    nodeURL:'http://localhost:7332'
+    
+});
 
 
 
@@ -43,6 +59,134 @@ let getEpochEdgeTransactionsFromMempool = epochFullID => {
 
 }
 
+
+
+let mockTestPairs = {
+
+    // Ed25519 keypairs
+
+    "9GQ46rqY238rk2neSwgidap9ww5zbAN4dyqyC7j5ZnBK": "MC4CAQAwBQYDK2VwBCIEILdhTMVYFz2GP8+uKUA+1FnZTEdN8eHFzbb8400cpEU9",
+    "6XvZpuCDjdvSuot3eLr24C1wqzcf2w4QqeDh9BnDKsNE": "MC4CAQAwBQYDK2VwBCIEIJT7NA/u+Df874H2DFRbyg43LpJwlhcRsS3Bv8/FUIZN",
+    "GUbYLN5NqmRocMBHqS183r2FQRoUjhx1p5nKyyUBpntQ": "MC4CAQAwBQYDK2VwBCIEILjvmDeOmyg1/VG2VKQTzsv6lkIizQpjmRsdfEEIHHU8",
+    "3JAeBnsMedzxjCMNWQYcAXtwGVE9A5DBQyXgWBujtL9R": "MC4CAQAwBQYDK2VwBCIEIDteWfNev7NOlNmwP8Irwg5miWKoErYGV+UU5VrFgYev",
+    "EGU4u3Anwahbtbx8F1ZZgFQSg2u49EkrkqMERT9r3q1o": "MC4CAQAwBQYDK2VwBCIEICVoiHLIICxjcuWQzq1vTLGJmaiU9fAOLEYKB9ZQR8TN",
+
+
+    // BLS keypairs
+
+    "0xb2ec32c9d7216163790ba3628a6a6b5a12db457c933b1f4627775b6dae468636233c6ad9931a8ef848a58353e60d33dd":"3981d303762bd2016644021e95052c50cb0916470a7eb36205bb12b97913523a",
+    "0x8f079049121d5e2ae885bdc6581df9fb68eab94a7aa3ae54bfe1d1ac35aceefbb202f656b0c1b56d64583630612a9970":"53f9079e2bcda99737d1024564ff422a18fcaf931059a3da76646dbbba85874b",
+};
+
+
+let postQuantumBlissKeypair = {
+
+    pubKey: '0012d71baf1524047e13c5006d00cf0cc3123e0ffe00941dda123a1c1806b50d261b660da60414067b13220793131b1d87099d0571175e0884092512c80d4308ab074e090502220c3519001ac10aad1126085e1c270cf815dc10dc04b508931a870b6619e0067e10cf0a7f1c3b04841452174400fc08ed0507040d1d39176b025b06d317e90057145017090e3907201dd50818020e0e74003504400a1a182c14f609f6117902981367191104050add14bd0b031af10c3e02a1160003011a5b137d00c8167b04521c4b1b9016250aeb01b7038d10a818da144406c91bca1b33195e0fd20930193e0dfa11f20f340da50b1215b51d21197c11060de009eb0c8201fb14110be00ec503bd065207a70953132d1a38115b153507da0a3e01290c8016af1d2c18a417100c1508cc112f146a130c013b014704471dbc02c20038013415621985124419ae10a501170eb70e6d0b220ee405ef17ff1c9b0dce0a1f07a204cf1b7b18b9013a0bdc00af187d169e050e0c201b5915c709b011db11170b06159b1cbe03691d860d00028d187d0e61074a1673027a047f16281bac0cfd09a00a62050c07ee1058020e006407de0adc1036136b10b417eb1b12155919b105f60b1d0bde0a57127b0007087d150c11690b7800930f1e16ec19ac0b8d1d7e1b0f02321c90148d1a47075a091113c9159e051113b403b5063001d3186e13b211c70d20',
+    privateKey: 'ba17dd98afb6dc1d13e4aed164d318d7921722f54e26496e1bc347213f0bd77230bf2b59676e7c346b155b660067ffb044a01dcc588ac2b6eb196bda8f981a70',
+    address: '4218fb0aaace62c4bfafbdd9adb05b99a9bf1a33eeae074215a51cb644b9a85c'
+
+}
+
+
+let nonces = {};
+
+let generateBatchOfMockTransactionsAndPushToMempool = async() => {
+
+    const shardID = 'shard_0';
+
+    const recipient = 'nXSYHp74u88zKPiRi7t22nv4WCBHXUBpGrVw3V93f2s';
+
+    for(let [pubKey,privateKey] of Object.entries(mockTestPairs)){
+
+        const from = pubKey;
+
+        const myPrivateKey = privateKey;
+
+        let nonce = nonces[pubKey] || await getUserAccountFromState(shardID+':'+pubKey).then(acc=>{
+
+            nonces[pubKey] = acc.nonce
+
+            return acc.nonce
+
+        })
+
+
+        nonce++
+
+        nonces[pubKey]++
+
+
+        const fee = 0.03;
+
+        const amountInKLY = 2;
+
+        let signedTx
+
+        if(pubKey.startsWith('0x')){
+
+            let payloadForMultisigTx = {
+
+                sigType:'M',
+
+                active:pubKey,
+        
+                afk:[],
+        
+                to:recipient,
+        
+                amount:amountInKLY
+
+            }
+
+            let dataToSign = BLOCKCHAIN_GENESIS.NETWORK_ID+WORKING_THREADS.VERIFICATION_THREAD.CORE_MAJOR_VERSION+'shard_0'+'TX'+JSON.stringify(payloadForMultisigTx)+nonce+fee
+
+            let signature = await bls.singleSig(dataToSign,privateKey)
+
+            signedTx = await web1337.createMultisigTransaction(from,from,signature,[],nonce,fee,recipient,amountInKLY)
+
+        } else {
+
+            signedTx = await web1337.createDefaultTransaction(shardID,from,myPrivateKey,nonce,recipient,fee,amountInKLY);
+
+        }
+
+        console.log(`TXID is => `,web1337.blake3(signedTx.sig))
+
+        NODE_METADATA.MEMPOOL.push(signedTx)
+    }
+
+    // Also, for tests, create tx with PQC account
+
+    const from = postQuantumBlissKeypair.address;
+
+    const myPrivateKey = postQuantumBlissKeypair.privateKey;
+
+    let nonce = nonces[from] || await getUserAccountFromState(shardID+':'+from).then(acc=>{
+
+        nonces[from] = acc.nonce
+
+        return acc.nonce
+
+    })
+
+
+    nonce++
+
+    nonces[from]++
+
+    const fee = 0.03;
+
+    const amountInKLY = 2;
+
+    let signedPqcTx = await web1337.createPostQuantumTransaction(shardID,'bliss',from,myPrivateKey,nonce,recipient,amountInKLY,fee)
+
+    console.log(`PQC TXID is => `,web1337.blake3(signedPqcTx.sig))
+
+    NODE_METADATA.MEMPOOL.push(signedPqcTx)
+
+}
+
+setInterval(generateBatchOfMockTransactionsAndPushToMempool,2000)
 
 
 
