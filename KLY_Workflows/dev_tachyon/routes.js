d50515914f46a9ed8843269d02db576487ba287b
VladChernenko <vladchernenko@protonmail.com>
2024-09-10 14:20:35 +0300

    fix(rename): API file renaming

--- KLY_Workflows/dev_tachyon/routes.js
+++ KLY_Workflows/dev_tachyon/routes.js
@@ -8,7 +8,7 @@ let modules = [
     'api/state_data.js',
     
     'internal_logic/epoch_changing.js',
-    'internal_logic/leaders_rotation.js',
+    'internal_logic/temp_vt_builder.js',
     'internal_logic/websocket.js',
     
     'kly_evm_json_rpc.js'
