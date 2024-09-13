76f270ede2e62d7cde4973f1b11afd5e31e96691
VladChernenko <vladchernenko@protonmail.com>
2024-09-13 16:01:32 +0300

    Fix contractID creation for RWX subcontracts

--- KLY_Workflows/dev_tachyon/system_contracts/rwx_contract.js
+++ KLY_Workflows/dev_tachyon/system_contracts/rwx_contract.js
@@ -85,7 +85,7 @@ export let CONTRACT = {
         // ...then - create a single storage for this new contract to store the body itself
         let futureRwxContractSingleStorage = transaction.payload.params[0]
 
-        let contractID = blake3Hash(originShard+JSON.stringify(transaction))
+        let contractID = blake3Hash(originShard+transaction.creator+transaction.nonce)
 
         
         // And put it to atomic batch to BLOCKCHAIN_DATABASES.STATE
