68ba083a640bb520487e6451e886c94082adf816
VladChernenko <vladchernenko@protonmail.com>
2024-11-24 12:39:34 +0200

    Fix

--- KLY_Workflows/dev_tachyon/verification_process/verification.js
+++ KLY_Workflows/dev_tachyon/verification_process/verification.js
@@ -1782,6 +1782,11 @@ let distributeFeesAmongPoolAndStakers = async(totalFees,blockCreatorPubKey) => {
 
     let currentEpochIndex = WORKING_THREADS.VERIFICATION_THREAD.EPOCH.id
 
+    if(!WORKING_THREADS.VERIFICATION_THREAD.ALLOCATIONS_PER_EPOCH[`${currentEpochIndex}`]){
+
+        WORKING_THREADS.VERIFICATION_THREAD.ALLOCATIONS_PER_EPOCH[`${currentEpochIndex}`] = {}
+
+    }
 
     WORKING_THREADS.VERIFICATION_THREAD.ALLOCATIONS_PER_EPOCH[`${currentEpochIndex}`]["mining"] += blockReward    
 
